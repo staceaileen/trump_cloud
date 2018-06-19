a362b0c42b483855ef397a334ff15f05ed0aa7c3
Stacy K <stacykam@mail.usf.edu>
2018-06-19 11:12:30 +0200

    got clean data

--- getting_tweets.R
+++ getting_tweets.R
@@ -2,36 +2,40 @@
 
 # importing packages
 library(twitteR)
-library(tidytext)library(dplyr)
+library(tidytext)
+library(dplyr)
 library(tidyr)
 library(stringr)
 library(ggplot2)
+library(RCurl)
+library(gutenbergr)
 
-<<<<<<< HEAD
 # configuration
 consumer_key <- "ul0zL1WmoQ9tKWgv0f5AZhiDn"
 consumer_secret <- "TJ3i0v6wNFx91nqfNNbVFSYyDC9vXp1QuOYo0gZ8HdiSTnDmcF"
 access_token <- "76037206-OCx8lq0rB3hiHz8WQfw9QRBv0S6ViEcZB8v9gUHcW"
 access_secret <- "OvTdlW1GHZ9MhBBypUq70gnSgfB5Pah13Bg7d5yh3wFmH"
-=======
-consumer_key <- ""
-consumer_secret <- ""
-access_token <- ""
-access_secret <- ""
->>>>>>> 0ed65c46138e4c1190e3b2e9106c0c0daca20b43
+
 
 # getting tweets using twitteR API
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
-<<<<<<< HEAD
+
 tw = twitteR::searchTwitter('@realDonaldTrump', n = 100, lang = 'en', since = '2017-01-08', retryOnRateLimit = 1e3)
 (d = twitteR::twListToDF(tw))
 
 tweet_stats = data.frame(tweet = d$text, favoriteCount = d$favoriteCount, retweetCount = d$retweetCount)
 tweet_loc = data.frame(tweet = d$text, lon = d$longitude, lat = d$latitude)
-tweet_created = data.frame(tweet = d$text, date = d$created)
+tweet_created = d %>% select(text, created)
+
+tweet_created$text <- gsub("@\\w+ *", "", tweet_created$text)
+tweet_created$text <- gsub("(R)T ", "", tweet_created$text)
+tweet_created$text <- gsub("https", "", tweet_created$text)
+tweet_created$text <- gsub("t.co", "", tweet_created$text)
 
 # cleaning the text from tweets
-=======
-tw = twitteR::searchTwitter('#realDonaldTrump + #HillaryClinton', n = 1e4, since = '2016-11-08', retryOnRateLimit = 1e3)
-d = twitteR::twListToDF(tw)
->>>>>>> 0ed65c46138e4c1190e3b2e9106c0c0daca20b43
+word_bag <- tweet_created %>% mutate(line = 1:nrow(.))
+(word_bag <- word_bag %>% unnest_tokens(word, text))
+
+data(stop_words)
+word_bag <- word_bag %>% anti_join(stop_words)
+
