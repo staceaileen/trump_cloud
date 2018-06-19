0d280fc8ff7e9e00fe2c7740e7ccbe8c707b2ae7
Stacy K <stacykam@mail.usf.edu>
2018-06-19 11:14:51 +0200

    god dammit lol

--- getting_tweets.R
+++ getting_tweets.R
@@ -11,10 +11,10 @@ library(RCurl)
 library(gutenbergr)
 
 # configuration
-consumer_key <- "ul0zL1WmoQ9tKWgv0f5AZhiDn"
-consumer_secret <- "TJ3i0v6wNFx91nqfNNbVFSYyDC9vXp1QuOYo0gZ8HdiSTnDmcF"
-access_token <- "76037206-OCx8lq0rB3hiHz8WQfw9QRBv0S6ViEcZB8v9gUHcW"
-access_secret <- "OvTdlW1GHZ9MhBBypUq70gnSgfB5Pah13Bg7d5yh3wFmH"
+consumer_key <- ""
+consumer_secret <- ""
+access_token <- ""
+access_secret <- ""
 
 
 # getting tweets using twitteR API
